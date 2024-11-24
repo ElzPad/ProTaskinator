bb94a27fd487cb059c684cb4b81c0bf130da6282
ElzPad <elziario.paduano@gmail.com>
2024-11-24 17:28:16 +0100

    Implemented useSignup hook

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,18 +1,21 @@
-import firebase from "firebase/app"
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "###",
-  authDomain: "###",
-  projectId: "###",
-  storageBucket: "###",
-  messagingSenderId: "###",
-  appId: "###"
+  apiKey: '###',
+  authDomain: '###',
+  projectId: '###',
+  storageBucket: '###',
+  messagingSenderId: '###',
+  appId: '###',
 };
 
-const app = firebase.initializeApp(firebaseConfig);       // Initialize Firebase
-const projectAuth = getAuth(app);                         // Initialize Auth
-const projectFirestore = getFirestore(app);               // Initialize Firestore
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Auth
+const projectAuth = getAuth(app);
+// Initialize Firestore
+const projectFirestore = getFirestore(app);
 
-export {projectAuth, projectFirestore}
+export { projectAuth, projectFirestore };
---
+++ src/hooks/useSignup.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import { projectAuth } from '../firebase/config';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+
+export const useSignup = () => {
+  const [isCancelled, setIsCancelled] = useState<boolean>(false);
+
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const signup = async (
+    email: string,
+    password: string,
+    displayName: string
+  ) => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const res = await createUserWithEmailAndPassword(
+        projectAuth,
+        email,
+        password
+      );
+      if (!res) throw new Error('Unable to complete signup');
+
+      if (projectAuth.currentUser)
+        updateProfile(projectAuth.currentUser, { displayName });
+
+      if (!isCancelled) {
+        setError(null);
+        setIsLoading(false);
+      }
+    } catch (err: any) {
+      if (!isCancelled) {
+        setError(err.message);
+        setIsLoading(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
+
+  return { signup, error, isLoading };
+};
