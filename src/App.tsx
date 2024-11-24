37a43370e2141cda966fb8141fe7641e919e6610
ElzPad <elziario.paduano@gmail.com>
2024-11-24 19:35:27 +0100

    Implemented route guarding

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,45 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
 
-import Navbar from './components/molecules/Navbar/Navbar'
-import Home from './pages/home/Home'
-import Signup from './pages/signup/Signup'
-import Login from './pages/login/Login'
-import Task from './pages/task/Task'
+import Navbar from './components/molecules/Navbar/Navbar';
+import Home from './pages/home/Home';
+import Signup from './pages/signup/Signup';
+import Login from './pages/login/Login';
+import Task from './pages/task/Task';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const { user, authIsReady } = useAuthContext();
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <div className="container">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/task/:id" element={<Task />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <div className="container">
+            <Navbar />
+            <Routes>
+              <Route
+                path="/"
+                element={user ? <Home /> : <Navigate to="/signup" />}
+              />
+              <Route
+                path="/task/:id"
+                element={user ? <Task /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/signup"
+                element={!user ? <Signup /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
