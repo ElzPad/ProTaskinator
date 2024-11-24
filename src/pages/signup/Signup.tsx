039adffd7416ce2b54d15ca4d95ff4a10b6b87a8
ElzPad <elziario.paduano@gmail.com>
2024-11-24 01:01:17 +0100

    Added Signup page

--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -1,7 +1,49 @@
+import { useState } from 'react'
 import './Signup.css'
 
 export default function Signup() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [displayName, setDisplayName] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(email, password, displayName);
+  }
+  
   return (
-    <div>Signup</div>
+    <>
+      <form className="authForm" onSubmit={handleSubmit}>
+        <h2>Signup</h2>
+        <label>
+          <span>Email:</span>
+          <input
+            type="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <label>
+          <span>Password:</span>
+          <input
+            type="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </label>
+        <label>
+          <span>Name:</span>
+          <input
+            type="text"
+            required
+            onChange={(e) => setDisplayName(e.target.value)}
+            value={displayName}
+          />
+        </label>
+        <button className="btn">Submit</button>
+      </form>
+    </>
   )
 }
