d2852e39f4ed5f40da75504f1e78249a5f1a95b8
ElzPad <elziario.paduano@gmail.com>
2024-12-11 23:03:49 +0100

    Added TaskTable in Home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,26 +1,16 @@
-import TaskCard from '../../components/molecules/TaskCard/TaskCard';
-import Modal from '../../components/organisms/Modal/Modal';
+import TaskTable from '../../components/organisms/TaskTable/TaskTable';
 import { useCollection } from '../../hooks/useCollection';
 import { Task } from '../../types/task';
 import './Home.css';
 
 export default function Home() {
-  const { documents, error } = useCollection<Task>('tasks');
+  const { documents, error, isLoading } = useCollection<Task>('tasks');
 
   return (
     <div>
-      <p>Home</p>
       {error && <div className="error">{error}</div>}
-      {documents &&
-        documents.map((task) => {
-          return (
-            <div key={task.id}>
-              <Modal status={task.status} title={task.title}>
-                <TaskCard taskInfo={task} />
-              </Modal>
-            </div>
-          );
-        })}
+      {isLoading && <div>Loading...</div>}
+      {documents && <TaskTable tasks={documents} />}
     </div>
   );
 }
