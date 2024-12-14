4992d6fd7a61d365fbdbdcc410c6fd47c620f66e
ElzPad <elziario.paduano@gmail.com>
2024-12-14 15:11:07 +0100

    Implemented Tag page

--- src/pages/tag/Tag.tsx
+++ src/pages/tag/Tag.tsx
@@ -1,7 +1,25 @@
-import './Tag.css';
 import { useParams } from 'react-router-dom';
+import TaskTable from '../../components/organisms/TaskTable/TaskTable';
+import { useCollection } from '../../hooks/useCollection';
+import { TaskType } from '../../types/task';
+import './Tag.css';
 
-export default function Tag() {
+export default function Home() {
   const { id } = useParams();
-  return <div>Tag #{id}</div>;
+  const { documents, error, isLoading } = useCollection<TaskType>('tasks');
+
+  return (
+    <div>
+      <h2 className="pageTitle">Tag: #{id}</h2>
+      {error && <div className="error">{error}</div>}
+      {isLoading && <div>Loading...</div>}
+      {documents && (
+        <TaskTable
+          tasks={documents.filter((d) => {
+            return d.tagsList.includes(id ? id : '');
+          })}
+        />
+      )}
+    </div>
+  );
 }
