a7d404b365ce0f02331091585760faa116902e73
ElzPad <elziario.paduano@gmail.com>
2024-12-14 15:00:10 +0100

    Implemented Task page

--- src/pages/task/Task.tsx
+++ src/pages/task/Task.tsx
@@ -1,10 +1,61 @@
-import { useParams } from 'react-router-dom'
-import './Task.css'
+import { Link, useParams } from 'react-router-dom';
+import './Task.css';
+import { useDocument } from '../../hooks/useDocument';
+import { TaskType } from '../../types/task';
+import InfoCard from '../../components/atoms/InfoCard/InfoCard';
 
 export default function Task() {
-  const {id} = useParams();
+  const { id } = useParams();
+  const { document, isLoading, error } = useDocument<TaskType>(
+    'tasks',
+    id ? id : ''
+  );
 
   return (
-    <div>Task {id}</div>
-  )
+    <div>
+      {isLoading && <div>Loading..</div>}
+      {error && <div className="error">{error}</div>}
+      {document && (
+        <>
+          <div style={{ display: 'flex', gap: '10px' }}>
+            <InfoCard label="Title">
+              <p>{document.title}</p>
+            </InfoCard>
+            <InfoCard label="Due date">
+              <p>{document.dueDate.toDate().toString()}</p>
+            </InfoCard>
+          </div>
+          <div>
+            <InfoCard label="Notes">
+              <p>{document.notes}</p>
+            </InfoCard>
+          </div>
+          <div style={{ display: 'flex', gap: '10px' }}>
+            <InfoCard label="People">
+              <p>{document.peopleList.join(', ')} </p>
+            </InfoCard>
+            <InfoCard label="Tags">
+              <ul>
+                {document.tagsList.map((tag) => {
+                  return (
+                    <div className="linkBtn" style={{ marginRight: '5px' }}>
+                      <Link to={`/tag/${tag}`}>#{tag}</Link>
+                    </div>
+                  );
+                })}
+              </ul>
+            </InfoCard>
+          </div>
+          <div style={{ display: 'flex', gap: '10px' }}>
+            <InfoCard label="Status">
+              <p>{document.status} </p>
+            </InfoCard>
+            <InfoCard label="Required Time">
+              <p>{document.requiredTime}</p>
+            </InfoCard>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
