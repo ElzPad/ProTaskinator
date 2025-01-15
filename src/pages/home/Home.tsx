68f04ab830e248676b976b45ea5ff3be51f73261
ElzPad <elziario.paduano@gmail.com>
2025-01-16 00:04:27 +0100

    Added selection for visualization type in the home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,19 +4,53 @@ import { useCollection } from '../../hooks/useCollection';
 import { TaskType } from '../../types/task';
 import './Home.css';
 import { useAuthContext } from '../../hooks/useAuthContext';
+import Select, { ActionMeta, SingleValue } from 'react-select';
+import { useState } from 'react';
+import TaskBoard from '../../components/organisms/TaskBoard/TaskBoard';
+import { SelectOptionsType } from '../../types/global';
 
 export default function Home() {
   const { user } = useAuthContext();
   const { documents, error, isLoading } = useCollection<TaskType>('tasks', [
     where('createdBy.uid', '==', user?.uid),
   ]);
+  const [visualization, setVisualization] = useState<SelectOptionsType | null>(
+    null
+  );
+
+  const handleChange = (
+    newValue: SingleValue<SelectOptionsType>,
+    _: ActionMeta<SelectOptionsType>
+  ) => {
+    setVisualization(newValue);
+  };
+
+  const visualizationOptions: SelectOptionsType[] = [
+    { value: 'Table', label: 'Table' },
+    { value: 'Board', label: 'Board' },
+  ];
 
   return (
     <div>
       <h2 className="pageTitle">Dashboard</h2>
       {error && <div className="error">{error}</div>}
       {isLoading && <div>Loading...</div>}
-      {documents && <TaskTable tasks={documents} />}
+      {documents && (
+        <div>
+          <label>
+            Visualization:
+            <Select
+              value={visualization}
+              options={visualizationOptions}
+              onChange={handleChange}
+            />
+          </label>
+          {(visualization == null || visualization?.value == 'Table') && (
+            <TaskTable tasks={documents} />
+          )}
+          {visualization?.value == 'Board' && <TaskBoard tasks={documents} />}
+        </div>
+      )}
     </div>
   );
 }
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,3 +1,8 @@
 export interface ChildrenProps {
   children: React.ReactNode;
 }
+
+export interface SelectOptionsType {
+  value: string;
+  label: string;
+}
