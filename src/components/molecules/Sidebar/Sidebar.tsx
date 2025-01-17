4504579515553a89eb875ad79695b3604f95d872
ElzPad <elziario.paduano@gmail.com>
2025-01-17 18:25:21 +0100

    Implemented Sidebar toggle

--- src/components/molecules/Sidebar/Sidebar.tsx
+++ src/components/molecules/Sidebar/Sidebar.tsx
@@ -2,31 +2,46 @@ import './Sidebar.css';
 import { useAuthContext } from '../../../hooks/useAuthContext';
 import { NavLink } from 'react-router-dom';
 import DashboardIcon from '../../../assets/dashboardIcon.svg';
+import RightArrowIcon from '../../../assets/rightArrowIcon.svg';
 import AddIcon from '../../../assets/addIcon.svg';
+import { useState } from 'react';
 
 export default function Sidebar() {
   const { user } = useAuthContext();
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  const toggleSidebar = () => {
+    setIsVisible((prevState) => {
+      return !prevState;
+    });
+  };
 
   return (
-    <div className="sidebar">
-      <div className="sidebarContent">
-        <div className="user">
-          <p>Hey {user ? user.displayName : 'user'}</p>
+    <div className={`sidebar ${isVisible ? 'open' : ''}`}>
+      {isVisible && (
+        <div className="sidebarContent">
+          <div className="user">
+            <p>Hey {user ? user.displayName : 'user'}</p>
+          </div>
+          <nav className="links">
+            <ul>
+              <li>
+                <NavLink to="/">
+                  <img src={DashboardIcon} alt="dashboard icon" />
+                  <span>Dashboard</span>
+                </NavLink>
+                <NavLink to="/create">
+                  <img src={AddIcon} alt="add project icon" />
+                  <span>New Task</span>
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <nav className="links">
-          <ul>
-            <li>
-              <NavLink to="/">
-                <img src={DashboardIcon} alt="dashboard icon" />
-                <span>Dashboard</span>
-              </NavLink>
-              <NavLink to="/create">
-                <img src={AddIcon} alt="add project icon" />
-                <span>New Task</span>
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
+      )}
+
+      <div className="toggleContainer" onClick={toggleSidebar}>
+        <img className="toggleIcon" src={RightArrowIcon} alt="Toggle sidebar" />
       </div>
     </div>
   );
