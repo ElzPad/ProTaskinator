c478b15540a6527e0a6e51b9af5029a00c09a8f3
ElzPad <elziario.paduano@gmail.com>
2025-01-17 20:05:50 +0100

    Implemented online status update during login and logout

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react';
-import { projectAuth } from '../firebase/config';
+import { projectAuth, projectFirestore } from '../firebase/config';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useAuthContext } from './useAuthContext';
+import { collection, doc, updateDoc } from 'firebase/firestore';
 
 export const useLogin = () => {
   const [isCancelled, setIsCancelled] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const { dispatch } = useAuthContext();
+  const { dispatch, user } = useAuthContext();
 
   const login = async (email: string, password: string) => {
     setIsLoading(true);
@@ -20,6 +21,11 @@ export const useLogin = () => {
         email,
         password
       );
+
+      const uid = res.user.uid;
+      const collectionRef = collection(projectFirestore, 'users');
+      const userRef = doc(collectionRef, uid);
+      await updateDoc(userRef, { online: true });
 
       if (!res) throw new Error('Unable to complete login');
 
--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,20 +1,26 @@
 import { signOut } from 'firebase/auth';
 import { useEffect, useState } from 'react';
-import { projectAuth } from '../firebase/config';
+import { projectAuth, projectFirestore } from '../firebase/config';
 import { useAuthContext } from './useAuthContext';
+import { collection, doc, updateDoc } from 'firebase/firestore';
 
 export const useLogout = () => {
   const [isCancelled, setIsCancelled] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const { dispatch } = useAuthContext();
+  const { dispatch, user } = useAuthContext();
 
-  const logout = () => {
+  const logout = async () => {
     setIsLoading(false);
     setError(null);
 
     try {
+      const uid = user?.uid;
+      const collectionRef = collection(projectFirestore, 'users');
+      const userRef = doc(collectionRef, uid);
+      await updateDoc(userRef, { online: false });
+
       const res = signOut(projectAuth);
 
       if (!res) throw new Error('Unable to complete logout');
