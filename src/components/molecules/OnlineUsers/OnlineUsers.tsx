c21b485ac22fcca06fd6934fd0071001f47ca991
ElzPad <elziario.paduano@gmail.com>
2025-01-18 16:00:47 +0100

    Implemented sliding transition for OnlineUsers sidebar

--- src/components/molecules/OnlineUsers/OnlineUsers.tsx
+++ src/components/molecules/OnlineUsers/OnlineUsers.tsx
@@ -5,6 +5,7 @@ import { useCollection } from '../../../hooks/useCollection';
 import { UserType } from '../../../types/user';
 import { useState } from 'react';
 import ProfileIcon from '../../../assets/profileIcon.svg';
+import { Link } from 'react-router-dom';
 
 export default function OnlineUsers() {
   const { error, documents } = useCollection<UserType>('users', []);
@@ -21,21 +22,22 @@ export default function OnlineUsers() {
       <div className="toggleContainer" onClick={toggleUsersList}>
         <img className="toggleIcon" src={ProfileIcon} alt="Toggle sidebar" />
       </div>
-      {isVisible && (
-        <div className="userListContent">
-          <h2>All users</h2>
 
-          {error && <div className="error">{error}</div>}
-          {documents &&
-            documents.map((user) => (
-              <div key={user.id} className="userListItem">
-                {user.online && <span className="onlineUser"></span>}
+      <div className="userListContent">
+        <h2>All users</h2>
+
+        {error && <div className="error">{error}</div>}
+        {documents &&
+          documents.map((user) => (
+            <div key={user.id} className="userListItem">
+              {user.online && <span className="onlineUser"></span>}
+              <Link to={`/chat/${user.id}`}>
                 <span>{user.displayName}</span>
-                <Avatar src={user.photoURL} />
-              </div>
-            ))}
-        </div>
-      )}
+              </Link>
+              <Avatar src={user.photoURL} />
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
