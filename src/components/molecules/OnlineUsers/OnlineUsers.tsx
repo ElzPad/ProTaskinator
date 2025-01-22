509497bc37e11803ba08e14c6c862de6905c29a5
ElzPad <elziario.paduano@gmail.com>
2025-01-22 19:59:24 +0100

    Implemented link to Chat in OnlineUsers component

--- src/components/molecules/OnlineUsers/OnlineUsers.tsx
+++ src/components/molecules/OnlineUsers/OnlineUsers.tsx
@@ -5,11 +5,14 @@ import { useCollection } from '../../../hooks/useCollection';
 import { UserType } from '../../../types/user';
 import { useState } from 'react';
 import ProfileIcon from '../../../assets/profileIcon.svg';
+import ChatIcon from '../../../assets/chatIcon.svg';
 import { Link } from 'react-router-dom';
+import { useAuthContext } from '../../../hooks/useAuthContext';
 
 export default function OnlineUsers() {
   const { error, documents } = useCollection<UserType>('users', []);
   const [isVisible, setIsVisible] = useState<boolean>(false);
+  const { user: loggedUser } = useAuthContext();
 
   const toggleUsersList = () => {
     setIsVisible((prevState) => {
@@ -27,16 +30,28 @@ export default function OnlineUsers() {
         <h2>All users</h2>
 
         {error && <div className="error">{error}</div>}
+
+        {loggedUser && (
+          <div className="userListItem">
+            <span className="onlineUser"></span>
+            <span>{`${loggedUser.displayName} (me)`}</span>
+            <Avatar src={loggedUser.photoURL ? loggedUser.photoURL : ''} />
+            <span className="noIcon"></span>
+          </div>
+        )}
         {documents &&
-          documents.map((user) => (
-            <div key={user.id} className="userListItem">
-              {user.online && <span className="onlineUser"></span>}
-              <Link to={`/chat/${user.id}`}>
+          documents
+            .filter((user) => user.id != loggedUser?.uid)
+            .map((user) => (
+              <div key={user.id} className="userListItem">
+                {user.online && <span className="onlineUser"></span>}
                 <span>{user.displayName}</span>
-              </Link>
-              <Avatar src={user.photoURL} />
-            </div>
-          ))}
+                <Avatar src={user.photoURL} />
+                <Link to={`/chat/${user.id}`}>
+                  <img src={ChatIcon} alt="Open chat" />
+                </Link>
+              </div>
+            ))}
       </div>
     </div>
   );
