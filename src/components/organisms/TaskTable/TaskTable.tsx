7a85f83a79c51da58bd8617b6fa78f245565d678
ElzPad <elziario.paduano@gmail.com>
2025-01-31 16:05:08 +0100

    Implemented status update from TaskTable component

--- src/components/organisms/TaskTable/TaskTable.tsx
+++ src/components/organisms/TaskTable/TaskTable.tsx
@@ -9,14 +9,25 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
-
 import ToDoIcon from '../../../assets/toDoIcon.svg';
 import InProgressIcon from '../../../assets/inProgressIcon.svg';
 import CompletedIcon from '../../../assets/completedIcon.svg';
+import { TaskType } from '../../../types/task';
+import { increment } from 'firebase/firestore';
+import { useFirestore } from '../../../hooks/useFirestore';
 
 export default function TaskTable(props: TaskTableProps) {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState<number>(10);
+
+  const icons: { [key: string]: string } = {
+    ToDo: ToDoIcon,
+    'In Progress': InProgressIcon,
+    Completed: CompletedIcon,
+  };
+
+  const { updateDocument: updateTask } = useFirestore('tasks');
+  const { updateDocument: updateProject } = useFirestore('projects');
 
   const handleChangePage = (
     e: React.MouseEvent<HTMLButtonElement> | null,
@@ -37,10 +48,37 @@ export default function TaskTable(props: TaskTableProps) {
     return text.slice(0, dim) + (text.length >= dim ? '...' : '');
   };
 
-  const icons: { [key: string]: string } = {
-    ToDo: ToDoIcon,
-    'In Progress': InProgressIcon,
-    Completed: CompletedIcon,
+  const setInProgress = (e: React.FormEvent, taskInfo: TaskType) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (taskInfo.id) {
+      if (taskInfo.projectId != '') {
+        updateProject(taskInfo.projectId, {
+          'progress.toDo': increment(-1),
+          'progress.inProgress': increment(1),
+        });
+      }
+      updateTask(taskInfo.id, {
+        status: 'In Progress',
+      });
+    }
+  };
+  const setCompleted = (e: React.FormEvent, taskInfo: TaskType) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (taskInfo.id) {
+      if (taskInfo.projectId != '') {
+        updateProject(taskInfo.projectId, {
+          'progress.inProgress': increment(-1),
+          'progress.completed': increment(1),
+        });
+      }
+      updateTask(taskInfo.id, {
+        status: 'Completed',
+      });
+    }
   };
 
   return (
@@ -55,6 +93,7 @@ export default function TaskTable(props: TaskTableProps) {
               <TableCell>Tags</TableCell>
               <TableCell>People</TableCell>
               <TableCell>Status</TableCell>
+              <TableCell>Update</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -88,6 +127,28 @@ export default function TaskTable(props: TaskTableProps) {
                     <img src={icons[t.status]} />
                     <span>{sliceText(t.status, 30)}</span>
                   </div>
+                </TableCell>
+                <TableCell>
+                  {t.status == 'ToDo' && (
+                    <div
+                      className="linkBtn"
+                      style={{ backgroundColor: 'var(--secondary-color)' }}
+                      onClick={(e) => setInProgress(e, t)}
+                    >
+                      Set "In Progress"
+                    </div>
+                  )}
+                  {t.status == 'In Progress' && (
+                    <div
+                      className="linkBtn"
+                      style={{ backgroundColor: 'var(--secondary-color)' }}
+                      onClick={(e) => {
+                        setCompleted(e, t);
+                      }}
+                    >
+                      Set "Completed"
+                    </div>
+                  )}
                 </TableCell>
                 <TableCell>
                   <div className="linkBtn">
